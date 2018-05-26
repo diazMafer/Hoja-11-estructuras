6b435434f78e31ae4e717f26ee8701e45ba789d7
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-05-25 19:33:45 -0600

    Fin

--- h11.py
+++ h11.py
@@ -10,13 +10,18 @@
 
 from modulo import *
 
-opcion = input("1.Calcular la ruta mas corta\n2.Calcular el centro del grafo\n0.Salir\n")
+opcion = input("1.Calcular la ruta mas corta\n2.Calcular el centro del grafo\n3.Agregar\n0.Salir\n")
 while(opcion!=0):
     if (opcion==1):
         ruta()
     if (opcion==2):
         centro()
-    opcion = input("1.Calcular la ruta mas corta\n2.Calcular el centro del grafo\n0.Salir\n")
+    if (opcion==3):
+        a = raw_input("Ingrese la ciudad de origin: ")
+        b = raw_input("Ingrese la ciudad de destino: ")
+        c = input("Ingrese los kilometros entre ruta: ")                      
+        agregar(a,b,c)
+    opcion = input("1.Calcular la ruta mas corta\n2.Calcular el centro del grafo\n3.Agregar\n0.Salir\n")
 
 
 
--- modulo.py
+++ modulo.py
@@ -10,27 +10,25 @@
 
 
 import networkx as nx
-
-
-Grafo = nx.DiGraph()
-
-archivo = open("ciudades.txt", "r")
-contenido = archivo.readlines()
-archivo.close()
-
-for lineas in contenido:
-    string = lineas.split(", ")
-    Grafo.add_node(string[0])
-    Grafo.add_node(string[1])
-    Grafo.add_edge(string[0],string[1],weight=float(string[2]))  #se le agrega un peso el cual sera los kilometros de cada uno 
-
-
-
-path = nx.floyd_warshall_predecessor_and_distance(Grafo) #se obtiene la ruta con el algoritmo de floyd
-Nodos = Grafo.nodes()
-
-
+        
 def ruta():
+    
+    Grafo = nx.DiGraph()
+
+    archivo = open("guategrafo.txt", "r")
+    contenido = archivo.readlines()
+    archivo.close()
+
+    for lineas in contenido:
+        string = lineas.split(", ")
+        Grafo.add_node(string[0])
+        Grafo.add_node(string[1])
+        Grafo.add_edge(string[0],string[1],weight=float(string[2]))  #se le agrega un peso el cual sera los kilometros de cada uno 
+
+
+    path = nx.floyd_warshall_predecessor_and_distance(Grafo) #se obtiene la ruta con el algoritmo de floyd
+    Nodos = Grafo.nodes()
+    
     print "Los ciudades son: "+"/".join(Nodos)
     
     ciudadO = raw_input("Ingrese la ciudad de la cual desea partir: ")
@@ -46,9 +44,29 @@ def ruta():
 
     print "La ruta mas corta entre ", ciudadO, " y ", ciudadF, " es pasando por: "
 
-    predecesor = path[0][ciudadO][ciudadF]
-    print predecesor
-        
+    predecesor1 = path[0][ciudadO][ciudadF]
+    ciudades = []
+    ciudades.append(predecesor1)
+    #print predecesor1
+    cont = 0
+    if predecesor1!=ciudadO:
+        while cont == 0:
+            predecesor = path[0][ciudadO][predecesor1]
+            ciudades.append(predecesor)
+            predecesor1 = predecesor
+            if predecesor1 == ciudadO:
+                cont = 1
+            else:
+                predecesor1 = predecesor
+                #print predecesor1
+
+    imprimir = []
+    for i in ciudades:
+        if ciudadO!=i:
+            imprimir.append(i)
+
+    print ciudadO, "---> "+"---> ".join(imprimir),"--->", ciudadF
+
     print "La distancia en kilometros es: "
     print path[1][ciudadO][ciudadF]
 
@@ -56,7 +74,7 @@ def ruta():
 
     if (bloqueos==1):
         print "Escriba desde donde comienza el bloqueo y hasta donde termina (nombre1, nombre2"
-        Edge = raw_input(Grafo.edges)
+        Edge = raw_input()
         Noditos = Edge.split(", ")
         if Noditos[0] or Noditos[1] not in Grafo.nodes():
             alerta = 1
@@ -66,21 +84,25 @@ def ruta():
             Noditos = Edge.split(", ")
 
         Grafo.remove_edge(Noditos[0],Noditos[1])
-        newpath = nx.floyd_warshall_predecessor_and_distance(Grafo, wight="peso")
+        newpath = nx.floyd_warshall_predecessor_and_distance(Grafo)
         print "El nuevo recorrido a tomar es: "
-        print newpath
+        print ciudadO, "-->", newpath[0][ciudadO][ciudadF],"-->", ciudadF
 
         print "Ruta en kilometros: "
         print newpath[1][ciudadO][ciudadF]
                                 
-
-
+def agregar(ciudadA, ciudadB, km):
+    archivo = open("guategrafo.txt", "a")
+    archivo.write(ciudadA + ", " + ciudadB + ", " + str(km) + "\n")
+    archivo.close()
+    
+    
 
 def centro():
-        #yo se que tiene que ser con dirección pero no funciona porque dice que no esta lo suficientemente conectado
+    #yo se que tiene que ser con dirección pero no funciona porque dice que no esta lo suficientemente conectado
     Grafo = nx.Graph()
 
-    archivo = open("ciudades.txt", "r")
+    archivo = open("guategrafo.txt", "r")
     contenido = archivo.readlines()
     archivo.close()
 
@@ -88,15 +110,12 @@ def centro():
         string = lineas.split(", ")
         Grafo.add_node(string[0])
         Grafo.add_node(string[1])
-        Grafo.add_edge(string[0],string[1],weight=int(string[2]))  #se le agrega un peso el cual sera los kilometros de cada uno 
+        Grafo.add_edge(string[0],string[1],weight=float(string[2]))  #se le agrega un peso el cual sera los kilometros de cada uno 
 
 
-
-    path = nx.floyd_warshall_predecessor_and_distance(Grafo) #se obtiene la ruta con el algoritmo de floyd
-    Nodos = Grafo.nodes()
     #centro del grafo
     centro = nx.center(Grafo, None, False)
-    print "Centro del grafo: ", centro
+    print "Centro del grafo: "+"/".join(centro)
 
 
     
